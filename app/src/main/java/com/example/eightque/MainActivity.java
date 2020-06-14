9561d4bda74b76585dc16203a36dc27d83e3ce46
Arfiya1234 <arfiya8801@gmail.com>
2020-06-14 16:58:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/eightque/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.eightque;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    protected void onResume() {
+        Toast.makeText( this, "onresume", Toast.LENGTH_SHORT).show();
+        super.onResume();
+
+    }
+    }
+
+
+
